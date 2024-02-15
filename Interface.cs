02b71df526c4c5f14384a568ff39e12133e562c7
Noga25 <nogale40@gmail.com>
2024-02-15 02:36:52 +0200

    Upade bag

--- Interface.cs
+++ Interface.cs
@@ -54,24 +54,20 @@ namespace Berzerkers2
         private readonly List<int> numbers;
         private readonly Random random;
 
-        // Constructor to initialize the Bag with all possible integer outcomes
         public Bag(int min, int max)
         {
             numbers = Enumerable.Range(min, max - min + 1).ToList();
             random = new Random();
         }
 
-        // Method to generate random numbers from the Bag
         public int Roll(uint min, uint max)
         {
-            // Shuffle the numbers if the Bag is empty
             if (numbers.Count == 0)
             {
                 numbers.AddRange(Enumerable.Range((int)min, (int)(max - min + 1)));
                 Shuffle(numbers);
             }
 
-            // Get and remove a random number from the Bag
             int index = random.Next(numbers.Count);
             int number = numbers[index];
             numbers.RemoveAt(index);
@@ -79,7 +75,6 @@ namespace Berzerkers2
             return number;
         }
 
-        // Method to shuffle the numbers in the Bag
         private void Shuffle(List<int> list)
         {
             int n = list.Count;
